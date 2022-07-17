2b54d7b302b6a90d5462a33e8405be9ead5edceb
Caique Brener <Caique.Brener-ext@ab-Inbev.com>
2022-07-16 22:28:56 -0300

    Refatorando nome da classe e adicionando a interface ao seu construtor

---
+++ app/src/main/java/com/caiquebrener/androidwithkoin/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.caiquebrener.androidwithkoin
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.caiquebrener.androidwithkoin.presenter.MySimplePresenter
+import org.koin.android.ext.android.inject
+
+class MainActivity : AppCompatActivity() {
+
+    private val firstPresenter: MySimplePresenter by inject()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        val myText = findViewById<TextView>(R.id.my_text)
+
+        myText.text = firstPresenter.sayHello()
+    }
+}
